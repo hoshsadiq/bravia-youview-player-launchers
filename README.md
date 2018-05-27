93310acc1af39de403fa0c3863ff53c11c38fae3
Hosh Sadiq <hoshsadiq@users.noreply.github.com>
2018-05-27 15:44:19 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# android-tv-youview-player-launchers
+
+This is a small application that simply adds a icon to the homescreen
+to be used with Sony TVs that run YouView:
+
+- BBC iPlayer
+- ITV Hub
+- All4
+- My5
+
+### Why
+
+I'm sure most Sony Android TV users will agree, the Sony Shelf is an
+unnecessary app, and without it, the above apps are not accessible
+without going through YouView.
+
+This will add icons to Android so that these apps can be launched
+without YouView. This does also mean you can disable YouView and still
+enjoy these player apps.
+
+### Installation
+
+Simply download the latest APK under [releases](https://github.com/hoshsadiq/android-tv-youview-player-launchers/releases)
+and sideload the app one way or another.
---
+++ app/src/main/java/hosh/io/atv_launchers/All4Activity.java
@@ -0,0 +1,29 @@
+package hosh.io.atv_launchers;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class All4Activity extends Activity {
+    // am start -a com.sony.dtv.hbbtvlauncher.LaunchHbbTV -n com.sony.dtv.hbbtvlauncher/.LaunchHbbTV
+    // -e HBBTV_LAUNCHER_INFO "type=bi, service=All4, url=https://yvweb.channel4.com/yvwebapp"
+
+    private String serviceName = "All4";
+    private String serviceUrl = "https://yvweb.channel4.com/yvwebapp";
+
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        startActivity(getYouViewIntent());
+        finish();
+    }
+
+    private Intent getYouViewIntent() {
+        Intent intent = new Intent();
+        intent.setClassName("com.sony.dtv.hbbtvlauncher", "com.sony.dtv.hbbtvlauncher.LaunchHbbTV");
+        intent.putExtra("HBBTV_LAUNCHER_INFO", "type=bi, " +
+                "service=" + serviceName + ", " +
+                "url=" + serviceUrl);
+
+        return intent;
+    }
+}
